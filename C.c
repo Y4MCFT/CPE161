4f854d7f7b5c7a5cb22c5d91ce401f13d83979d2
YaMx <xxiamyam@gmail.com>
2022-11-01 22:56:09 +0700

    C input number (circle)

---
+++ C.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main(){
+	int x ,y ,sum;
+	printf("Enter value of x : ");
+	scanf("%d" ,&x);
+	printf("Enter valur of y : ");
+	scanf("%d" ,&y);
+	sum = 3.14 * (x * y);
+	printf("Sum of curcle : %d ",sum);
+	return 0;
+}
